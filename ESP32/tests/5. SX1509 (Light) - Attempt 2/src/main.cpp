ee1cb9e87576ad309c5e87891af0c381da5c0aba
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2021-01-22 16:57:49 +0000

    next example

---
+++ ESP32/tests/5. SX1509 (Light) - Attempt 2/src/main.cpp
@@ -0,0 +1,56 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include "SN7 pins.h"
+#include <SparkFunSX1509.h> // Include SX1509 library
+
+const byte SX1509_ADDRESS = 0x3E; // SX1509 I2C address
+SX1509 sx1509;                    // Create an SX1509 object to be used throughout
+
+void setup()
+{
+  Wire.begin(SDA, SCL); //I2C bus
+  Serial.begin(115200); //ESP32 USB Port
+
+  if (!sx1509.begin(SX1509_ADDRESS))
+  {
+    Serial.println("SX1509 not found");
+  }
+  else
+  {
+    Serial.println("SX1509 lighting found");
+
+    sx1509.clock(INTERNAL_CLOCK_2MHZ, 4);
+
+    // for (size_t pin = 0; pin < 16; pin++)
+    // {
+    int pin = 7;
+
+    sx1509.pinMode(pin, ANALOG_OUTPUT);  // To breathe an LED, make sure you set it as an ANALOG_OUTPUT, so we can PWM the pin
+    sx1509.breathe(pin, 0, 0, 500, 500); // Breathe an LED: 1000ms LOW, 500ms HIGH, 500ms to rise from low to high, 250ms to fall from high to low
+
+    pin = 15;
+
+    sx1509.pinMode(pin, OUTPUT);
+    sx1509.blink(pin, 1000, 1000);
+    // }
+  }
+
+  delay(5000);
+
+  sx1509.begin(SX1509_ADDRESS);
+
+  int pin = 7;
+
+  sx1509.pinMode(pin, ANALOG_OUTPUT);    // To breathe an LED, make sure you set it as an ANALOG_OUTPUT, so we can PWM the pin
+  sx1509.breathe(pin, 0, 0, 1000, 1000); // Breathe an LED: 1000ms LOW, 500ms HIGH, 500ms to rise from low to high, 250ms to fall from high to low
+
+  pin = 15;
+
+  sx1509.pinMode(pin, OUTPUT);
+  sx1509.blink(pin, 1000, 1000);
+}
+
+void loop()
+{
+  delay(1000);
+}
